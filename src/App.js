75bd5b7df73320d7f913d359206c677109a54dc6
Meelan Magar <mmggrr839@gmail.com>
2025-09-23 14:45:11 +0545

    feat: done

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx, Fragment as _Fragment } from "@emotion/react/jsx-runtime";
-import "./App.css";
-import Todo from "./components/Todo";
-function App() {
-    return (_jsx(_Fragment, { children: _jsx(Todo, {}) }));
-}
-export default App;
--- src/components/Form.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "@emotion/react/jsx-runtime";
-import { buttonStyle, form, input } from "./Todo.style";
-const Form = ({ inputValue, setInputValue, handleAddTodo, }) => {
-    return (_jsx("div", { children: _jsxs("form", { css: form, onSubmit: handleAddTodo, children: [_jsx("input", { css: input, type: "text", value: inputValue, onChange: (e) => setInputValue(e.target.value), placeholder: "Enter your todo" }), _jsx("button", { css: buttonStyle("blue", "darkblue", "120px", "50px", "16px"), type: "submit", children: "Add" })] }) }));
-};
-export default Form;
